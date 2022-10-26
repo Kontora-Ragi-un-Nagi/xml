a7d3bca7b4a36a53fa1ceb1f20a452ae9b650ab5
Aleksejs Semjonovs <yelowcat@gmail.com>
2022-10-26 17:21:50 +0300

    Add project files.

---
+++ xml/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xml
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+        public Person(string name, int age, string company)
+        {
+            Name = name;
+            Age = age;
+            Company = company;
+        }
+
+        public Person()
+        {
+
+        }
+    }
+}
