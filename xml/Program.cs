38a597d54ce426a4f806fc5e283b4a8752a8d11a
Jānis Stankevičs <students.stankevics@macibucentrs.onmicrosoft.com>
2022-10-28 22:12:29 +0300

    FIX: XML file save without Microsoft

--- xml/Program.cs
+++ xml/Program.cs
@@ -4,232 +4,30 @@ using System.Xml.Linq;
 using System.Security.Cryptography;
 using System;
 
-
-#region Описание
-//XmlNode: представляет узел xml.В качестве узла может использоваться весь документ, так и отдельный элемент
-
-//XmlDocument: представляет весь xml-документ
-
-//XmlElement: представляет отдельный элемент. Наследуется от класса XmlNode
-
-//XmlAttribute: представляет атрибут элемента
-
-//XmlText: представляет значение элемента в виде текста, то есть тот текст, который находится в элементе между его открывающим и закрывающим тегами
-
-//XmlComment: представляет комментарий в xml
-
-//XmlNodeList: используется для работы со списком узлов 
-#endregion
-
 var people = new List<Person>();
 
-//#region Чтение XML и интерпретация
-//XmlDocument xDoc = new XmlDocument();
-//xDoc.Load("people.xml");
-//// получим корневой элемент
-//XmlElement? xRoot = xDoc.DocumentElement;
-//if (xRoot != null)
-//{
-//    foreach (XmlElement xnode in xRoot)
-//    {
-//        Person person = new Person();
-//        XmlNode? attr = xnode.Attributes.GetNamedItem("name");
-//        person.Name = attr?.Value;
 
-//        foreach (XmlNode childn in xnode.ChildNodes)
-//        {
-//            if (childn.Name == "company")
-//                person.Company = childn.InnerText;
-
-//            if (childn.Name == "age")
-//                person.Age = int.Parse(childn.InnerText);
-//        }
-//        people.Add(person);
-//    }
-//    foreach (var person in people)
-//        Console.WriteLine($"{person.Name} ({person.Company}) - {person.Age}");
-//}
-//#endregion
-
-//#region Добавление узла
-//// создаем новый элемент person
-//XmlElement personElem = xDoc.CreateElement("person");
-
-//// создаем атрибут name
-//XmlAttribute nameAttr = xDoc.CreateAttribute("name");
-
-//// создаем элементы company и age
-//XmlElement companyElem = xDoc.CreateElement("company");
-//XmlElement ageElem = xDoc.CreateElement("age");
-
-//// создаем текстовые значения для элементов и атрибута
-//XmlText nameText = xDoc.CreateTextNode("Mark");
-//XmlText companyText = xDoc.CreateTextNode("Facebook");
-//XmlText ageText = xDoc.CreateTextNode("30");
-
-////добавляем узлы
-//nameAttr.AppendChild(nameText);
-//companyElem.AppendChild(companyText);
-//ageElem.AppendChild(ageText);
-
-//// добавляем атрибут name
-//personElem.Attributes.Append(nameAttr);
-//// добавляем элементы company и age
-//personElem.AppendChild(companyElem);
-//personElem.AppendChild(ageElem);
-//// добавляем в корневой элемент новый элемент person
-//xRoot?.AppendChild(personElem);
-//// сохраняем изменения xml-документа в файл
-//xDoc.Save("people.xml");
-//#endregion
-
-//#region Удаление узла
-//XmlNode? firstNode = xRoot?.FirstChild;
-//if (firstNode is not null) xRoot?.RemoveChild(firstNode);
-//xDoc.Save("people.xml");
-//#endregion
-
-//#region XPath - язык запросов у XML
-////.
-
-////выбор текущего узла
-
-////..
-
-////выбор родительского узла
-
-////*
-
-////выбор всех дочерних узлов текущего узла
-
-////person
-
-////выбор всех узлов с определенным именем, в данном случае с именем "person"
-
-////@name
-
-////выбор атрибута текущего узла, после знака @ указывается название атрибута (в данном случае "name")
-
-////@+
-
-////выбор всех атрибутов текущего узла
-
-////element[3]
-
-////выбор определенного дочернего узла по индексу, в данном случае третьего узла
-
-//////person
-
-////выбор в документе всех узлов с именем "person"
-
-////person[@name='Tom']
-
-////выбор элементов с определенным значением атрибута. В данном случае выбираются все элементы "person" с атрибутом name='Tom'
-
-////person[company='Microsoft']
-
-////выбор элементов с определенным значением вложенного элемента. В данном случае выбираются все элементы "person", у которых дочерний элемент "company" имеет значение 'Microsoft'
-
-//////person/company
-
-////выбор в документе всех узлов с именем "company", которые находятся в элементах "person"
-//#endregion
-
-//#region Пример запросов
-
-//XmlNodeList? personNodes = xRoot?.SelectNodes("person");
-//if (personNodes is not null)
-//{
-//    foreach (XmlNode node in personNodes)
-//        Console.WriteLine(node.SelectSingleNode("@name")?.Value);
-//}
-
-//XmlNodeList? nodes = xRoot?.SelectNodes("*");
-//if (nodes is not null)
-//{
-//    foreach (XmlNode node in nodes)
-//        Console.WriteLine(node.OuterXml);
-//}
-
-//XmlNode childnode = xRoot.SelectSingleNode("person[@name='Tom']");
-//if (childnode != null)
-//    Console.WriteLine(childnode.OuterXml);
-
-//XmlNode? tomNode = xRoot?.SelectSingleNode("person[@name='Tom']");
-//Console.WriteLine(tomNode?.OuterXml);
-
-//XmlNodeList? companyNodes = xRoot?.SelectNodes("//person/company");
-//if (companyNodes is not null)
-//{
-//    foreach (XmlNode node in companyNodes)
-//        Console.WriteLine(node.InnerText);
-//}
-
-//#endregion
-
-#region LinqToXML
-
-//Это иной подход и другой набор классов
-
-//XAttribute: представляет атрибут xml - элемента
-
-//XComment: представляет комментарий
-
-//XDocument: представляет весь xml - документ
-
-//XElement: представляет отдельный xml - элемент
-
-//Ключевым классом является XElement, который позволяет получать вложенные элементы и управлять ими. Среди его методов можно отметить следующие:
-
-//Add(): добавляет новый атрибут или элемент
-
-//Attributes(): возвращает коллекцию атрибутов для данного элемента
-
-//Elements(): возвращает все дочерние элементы данного элемента
-
-//Remove(): удаляет данный элемент из родительского объекта
-
-//RemoveAll(): удаляет все дочерние элементы и атрибуты у данного элемента
-
-//XDocument xdoc = new XDocument(new XElement("people",
-//new XElement("person",
-//new XAttribute("name", "Tom"),
-//new XElement("company", "Microsoft"),
-//new XElement("age", 37)),
-//new XElement("person",
-//new XAttribute("name", "Bob"),
-//new XElement("company", "Google"),
-//new XElement("age", 41))));
-
-//PART I
+//PART I - Microsoft darbinieklu vidējais vecums
 
 XDocument xdoc = XDocument.Load("input.xml");
 
 XElement? peoples = xdoc.Element("people");
 
 if (people is not null)
-{
-    // проходим по всем элементам person
+{    
     foreach (XElement person in peoples.Elements("person"))
     {
 
         XAttribute? name = person.Attribute("name");
         XElement? company = person.Element("company");
-        XElement? age = person.Element("age");
-
-        //Console.WriteLine($"Person: {name?.Value}");
-        //Console.WriteLine($"Company: {company?.Value}");
-        //Console.WriteLine($"Age: {age?.Value}");
-
-        //Console.WriteLine(); //  для разделения при выводе на консоль
+        XElement? age = person.Element("age"); 
     }
 }
 
-var microsoft = xdoc.Element("people")?   // получаем корневой узел people
-    .Elements("person")             // получаем все элементы person
-                                    // получаем все person, у которого company = Microsoft
+var microsoft = xdoc.Element("people")?
+    .Elements("person")                                    
     .Where(p => p.Element("company")?.Value == "Microsoft")
-    .Select(p => new        // для каждого объекта создаем анонимный объект
+    .Select(p => new
     {
         name = p.Attribute("name")?.Value,
         age = p.Element("age")?.Value,
@@ -243,9 +41,7 @@ if (microsoft is not null)
     int ages = 0;
     foreach (var person in microsoft)
     {
-
         x = x + 1;
-
         int m = Int32.Parse(person.age);
         ages = ages + m;
         Console.WriteLine($"Name: {person.name}  Age: {person.age}");
@@ -257,31 +53,25 @@ if (microsoft is not null)
     Console.WriteLine();
     Console.WriteLine();
 
-    // PART II
+    // PART II - MCPlus darbinieki
 
     if (people is not null)
-    {
-        // проходим по всем элементам person
+    {    
         foreach (XElement person in peoples.Elements("person"))
         {
 
             XAttribute? name = person.Attribute("name");
             XElement? company = person.Element("company");
             XElement? age = person.Element("age");
-
-            //Console.WriteLine($"Person: {name?.Value}");
-            //Console.WriteLine($"Company: {company?.Value}");
-            //Console.WriteLine($"Age: {age?.Value}");
-
-            //Console.WriteLine(); //  для разделения при выводе на консоль
+     
         }
     }
 
-    var mcplus = xdoc.Element("people")?   // получаем корневой узел people
-        .Elements("person")             // получаем все элементы person
-                                        // получаем все person, у которого company = Microsoft
+    var mcplus = xdoc.Element("people")?
+        .Elements("person")
+                                        
         .Where(p => p.Element("company")?.Value == "MCPlus")
-        .Select(p => new        // для каждого объекта создаем анонимный объект
+        .Select(p => new
         {
             name = p.Attribute("name")?.Value,
             age = p.Element("age")?.Value,
@@ -299,31 +89,25 @@ if (microsoft is not null)
     Console.WriteLine();
     Console.WriteLine();
 
-    // PART III
+    // PART III - Visi Google un MCPlus darbinieki, viņu vidējais vecums, XML fails ar šo kompāniju darbiniekiem
 
     if (people is not null)
-    {
-        // проходим по всем элементам person
+    {        
         foreach (XElement person in peoples.Elements("person"))
         {
 
             XAttribute? name = person.Attribute("name");
             XElement? company = person.Element("company");
             XElement? age = person.Element("age");
-
-            //Console.WriteLine($"Person: {name?.Value}");
-            //Console.WriteLine($"Company: {company?.Value}");
-            //Console.WriteLine($"Age: {age?.Value}");
-
-            //Console.WriteLine(); //  для разделения при выводе на консоль
+            
         }
     }
 
-    var google = xdoc.Element("people")?   // получаем корневой узел people
-        .Elements("person")             // получаем все элементы person
-                                        // получаем все person, у которого company = Microsoft
+    var google = xdoc.Element("people")?
+        .Elements("person")
+                                        
         .Where(p => p.Element("company")?.Value == "Google")
-        .Select(p => new        // для каждого объекта создаем анонимный объект
+        .Select(p => new
         {
             name = p.Attribute("name")?.Value,
             age = p.Element("age")?.Value,
@@ -367,5 +151,13 @@ if (microsoft is not null)
         Console.WriteLine("Vidējais MCPlus un Google darbinieku vecums: " + s + " gadi.");
     }
 
+    
+    // Google un MCPlus darbinieki tiek saglabāti jaunā XML failā
+    
+    peoples.DescendantsAndSelf("person")
+       .Where(r => (string)r.Element("company").Value == "Microsoft")
+       .Remove();
+    
+    xdoc.Save("JanisXML.xml");
+
 }
-#endregion
